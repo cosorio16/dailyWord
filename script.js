eb1806f1c73e70cb5a095790a5e357d473b7ea5d
cosorio16 <carlososorio1230@gmail.com>
2024-04-08 22:05:03 -0500

    Functions complete,verification of words by columns and reboot after win. Word of example: osojs

--- script.js
+++ script.js
@@ -1,53 +1,71 @@
+import { wordsFiveLetters as WORDS_GAME } from "./words.js";
+
 const $box = document.querySelectorAll(".box");
 
+function randomWord() {
+  const word = WORDS_GAME[Math.floor(Math.random() * WORDS_GAME.length)];
+  return word;
+}
+
+let round = 1;
+let roundPosition = 0;
+let word = "money";
+
 let currentPosition = 0;
-const word = "plate";
-const letras = word.split("");
+let letras = word.split("");
+let statusWord = [];
 
 function compare() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = roundPosition; i < 5 * round; i++) {
     $box[i].classList.remove("bg-rojo", "bg-verde");
-    $box[i].textContent.trim() == letras[i]
+    $box[i].textContent.trim() == letras[i - roundPosition]
       ? $box[i].classList.add("bg-verde", "text-blanco")
       : $box[i].classList.add("bg-rojo", "text-blanco");
+
+    statusWord.push($box[i].textContent.trim() == letras[i - roundPosition]);
   }
 }
 
 function writeWord(letter) {
-  if (currentPosition < 5) {
+  if (currentPosition < 5 * round) {
     const currentLetter = ($box[currentPosition].innerHTML = letter);
     currentPosition++;
   }
-  console.log(currentPosition);
 }
 
 function deleteLetter(i) {
-  if (currentPosition > 0) {
+  if (currentPosition > roundPosition) {
     $box[i].innerHTML = "";
     $box[i].classList.remove("bg-rojo", "bg-verde");
     currentPosition--;
-    console.log(currentPosition);
   }
 }
 
 function clearAll() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5 * round; i++) {
     $box[i].innerHTML = "";
     $box[i].classList.remove("bg-rojo", "bg-verde");
   }
+  round = 1;
+  roundPosition = 0;
   currentPosition = 0;
 }
 
 document.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
+  if (currentPosition == 5 * round && e.key == "Enter") {
     compare();
+    roundPosition = roundPosition + 5;
+    round++;
+    if (!statusWord.includes(false)) {
+      alert(`Palabra encontrada: ${word}`);
+      setTimeout(() => {
+        clearAll();
+      }, 1000);
+    }
+    statusWord = [];
   } else if (e.key == e.key.match(/[a-z]/i)) {
     writeWord(e.key);
   } else if (e.key == "Backspace") {
     deleteLetter(currentPosition - 1);
-  } else if (e.key == "Shift") {
-    clearAll();
   }
-
-  console.log(e.key);
 });

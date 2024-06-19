77fb1209ca2013ab90636d2d1efd6336eac85c1d
cosorio16 <carlososorio1230@gmail.com>
2024-06-19 12:58:22 -0500

    keyboard.js added to generate a bottom keyboard functional, first version deploy

---
+++ keyboard.js
@@ -0,0 +1,43 @@
+const $keyboardRows = document.querySelectorAll(".keyboard_row");
+
+const firstRow = "qwertyuiop";
+const secondRow = "asdfghjklñ";
+const thirdRow = "zxcvbnm";
+const deleteIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+    <g fill="none" stroke="currentColor" stroke-width="1.5">
+      <path d="M11.142 20c-2.227 0-3.341 0-4.27-.501c-.93-.502-1.52-1.42-2.701-3.259l-.681-1.06C2.497 13.634 2 12.86 2 12c0-.86.497-1.634 1.49-3.18l.68-1.06c1.181-1.838 1.771-2.757 2.701-3.259C7.801 4 8.915 4 11.142 4h2.637c3.875 0 5.813 0 7.017 1.172C22 6.343 22 8.229 22 12c0 3.771 0 5.657-1.204 6.828C19.592 20 17.654 20 13.78 20z"/>
+      <path stroke-linecap="round" d="m15.5 9.5l-5 5m0-5l5 5"/>
+    </g>
+  </svg>
+`;
+const createButton = (content, isHTML = false, className = "") => {
+  const button = document.createElement("button");
+  if (className) {
+    button.classList.add(className);
+  }
+  if (isHTML) {
+    button.innerHTML = content;
+  } else {
+    button.textContent = content.toUpperCase();
+  }
+  return button;
+};
+
+firstRow.split("").forEach((l) => {
+  $keyboardRows[0].appendChild(createButton(l, false, 'keyLetter'));
+});
+
+secondRow.split("").forEach((l) => {
+  $keyboardRows[1].appendChild(createButton(l, false, 'keyLetter'));
+});
+
+const enterButton = createButton("Enter", false, "enter");
+$keyboardRows[2].appendChild(enterButton);
+
+thirdRow.split("").forEach((l) => {
+  $keyboardRows[2].appendChild(createButton(l, false, 'keyLetter'));
+});
+
+const deleteButton = createButton(deleteIcon, true, "delete");
+$keyboardRows[2].appendChild(deleteButton);
--- script.js
+++ script.js
@@ -1,84 +1,122 @@
-const $boxes = document.querySelectorAll(".box");
+document.addEventListener("DOMContentLoaded", () => {
+  const $boxes = document.querySelectorAll(".box");
+  const $letterButtons = document.querySelectorAll(".keyLetter");
+  const $deleteKeyboard = document.querySelector(".delete");
+  const $enterKeyboard = document.querySelector(".enter");
 
-let word = "polvo";
-let letters = word.split("");
-
-let position = 0;
-let round = 1;
-const limit = letters.length;
-
-function writeLetter(letter) {
-  if (position < limit * round) {
-    $boxes[position].textContent = letter.toLowerCase();
-  }
-  position < limit * round - 1 && position++;
-}
-
-function deleteWord() {
-  $boxes[position].textContent = "";
-  if (position > limit * (round - 1) && position < limit * round) {
-    position--;
-  }
-}
-
-function compare() {
-  let guessLetter = [];
-
-  for (let i = limit * (round - 1); i < limit * round; i++) {
-    if ($boxes[i].textContent == letters[i - limit * (round - 1)]) {
-      $boxes[i].classList.add("correct");
-      guessLetter.push($boxes[i].textContent);
-    } else {
-      $boxes[i].classList.add("incorrect");
-    }
-  }
-
-  for (let i = limit * (round - 1); i < limit * round; i++) {
+  $enterKeyboard.addEventListener("click", () => {
     if (
-      letters.includes($boxes[i].textContent) &&
-      guessLetter.filter((l) => l == $boxes[i].textContent).length <
-        letters.filter((l) => l == $boxes[i].textContent).length
+      position == limit * round - 1 &&
+      $boxes[limit * round - 1].textContent != ""
     ) {
-      $boxes[i].classList.replace("incorrect", "unknown");
-
-      guessLetter.push($boxes[i].textContent);
+      compare();
+      round++;
+      position = word.length * (round - 1);
     }
-  }
-
-  console.log(guessLetter);
-}
-
-function resetGame() {
-  position = 0;
-  round = 1;
-  $boxes.forEach((box) => {
-    box.textContent = "";
-    box.classList.remove("correct", "incorrect", "unknown");
   });
-}
 
-document.addEventListener("keydown", (e) => {
-  if (e.key == e.key.match(/[a-z]/i)) {
-    writeLetter(e.key);
-  } else if (e.key == "Backspace") {
+  $deleteKeyboard.addEventListener("click", () => {
     deleteWord();
-  } else if (
-    e.key == "Enter" &&
-    position == limit * round - 1 &&
-    $boxes[limit * round - 1].textContent != ""
-  ) {
-    compare();
-    round++;
-    position = word.length * (round - 1);
-  } else {
-    console.log(e.key);
+    activeLetter();
+  });
+
+  $letterButtons.forEach((b) => {
+    b.addEventListener("click", () => {
+      writeLetter(b.textContent);
+    });
+  });
+
+  let word = "money";
+  let letters = [];
+
+  let entryWord = word.split("");
+  entryWord.map((l) => {
+    letters.push(l.toUpperCase());
+  });
+
+  let position = 0;
+  let round = 1;
+  const limit = letters.length;
+
+  function activeLetter() {
+    $boxes.forEach((box, index) => {
+      if (index == position) {
+        box.classList.add("active");
+      } else {
+        box.classList.remove("active");
+      }
+    });
   }
 
-  $boxes.forEach((box, index) => {
-    if (index == position) {
-      box.classList.add("active");
-    } else {
-      box.classList.remove("active");
+  function writeLetter(letter) {
+    if (position < limit * round) {
+      $boxes[position].textContent = letter.toUpperCase();
     }
+    position < limit * round - 1 && position++;
+    activeLetter();
+  }
+
+  function deleteWord() {
+    $boxes[position].textContent = "";
+    if (position > limit * (round - 1) && position < limit * round) {
+      position--;
+    }
+  }
+
+  function compare() {
+    let guessLetter = [];
+
+    for (let i = limit * (round - 1); i < limit * round; i++) {
+      if ($boxes[i].textContent == letters[i - limit * (round - 1)]) {
+        $boxes[i].classList.add("correct");
+        guessLetter.push($boxes[i].textContent);
+      } else {
+        $boxes[i].classList.add("incorrect");
+      }
+    }
+
+    for (let i = limit * (round - 1); i < limit * round; i++) {
+      if (
+        letters.includes($boxes[i].textContent) &&
+        guessLetter.filter((l) => l == $boxes[i].textContent).length <
+          letters.filter((l) => l == $boxes[i].textContent).length
+      ) {
+        if ($boxes[i].classList.contains("incorrect")) {
+          $boxes[i].classList.replace("incorrect", "unknown");
+          guessLetter.push($boxes[i].textContent);
+        }
+      }
+    }
+
+    console.log(guessLetter);
+  }
+
+  function resetGame() {
+    position = 0;
+    round = 1;
+    $boxes.forEach((box) => {
+      box.textContent = "";
+      box.classList.remove("correct", "incorrect", "unknown");
+    });
+  }
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key == e.key.match(/[a-z]/i)) {
+      writeLetter(e.key);
+    } else if (e.key == "Backspace") {
+      deleteWord();
+    } else if (
+      e.key == "Enter" &&
+      position == limit * round - 1 &&
+      $boxes[limit * round - 1].textContent != ""
+    ) {
+      compare();
+      round++;
+      position = word.length * (round - 1);
+    } else {
+      console.log(e.key);
+    }
+
+    activeLetter();
   });
 });
